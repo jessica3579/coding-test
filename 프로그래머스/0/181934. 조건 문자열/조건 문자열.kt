37dc9bdad5be9ae111e388b7d377b841b03c76b4
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-04-24 21:27:47 +0900

    [level 0] Title: 조건 문자열, Time: 1.99 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.kt
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.kt
@@ -1,14 +1,13 @@
 class Solution {
     fun solution(ineq: String, eq: String, n: Int, m: Int): Int {
-        if(n == m){
-            if(eq == "=") return 1
-            else return 0
-        }else if(n > m){
-            if(ineq == ">") return 1
-            else return 0
-        }else{
-            if(ineq == "<") return 1
-            else return 0
+        var answer: Int = 0
+        val op = ineq + eq
+        when(op){
+            ">=" -> answer = if(n>=m) 1 else 0
+            "<=" -> answer = if(n<=m) 1 else 0
+            ">!" -> answer = if(n>m) 1 else 0
+            "<!" -> answer = if(n<m) 1 else 0
         }
+        return answer
     }
 }
