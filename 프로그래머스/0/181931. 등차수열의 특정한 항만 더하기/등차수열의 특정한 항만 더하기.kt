a1f714ec9729ac991020d3d4820338ee4562984f
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-04-24 22:39:19 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(a: Int, d: Int, included: BooleanArray): Int {
+        var answer: Int = 0
+        for(i in included.indices){
+            if(included[i] == true) answer += a+i*d
+        }
+        return answer
+    }
+}
