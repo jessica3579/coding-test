d3ce5adbea4aa3d5fc3ce340a976662bad85d17e
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-04-24 17:30:11 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.kt
+++ 프로그래머스/0/181936. 공배수/공배수.kt
@@ -1,8 +1,5 @@
 class Solution {
     fun solution(number: Int, n: Int, m: Int): Int {
-        val isN: Boolean = number % n == 0
-        val isM: Boolean = number % m == 0
-        if(isN && isM) return 1
-        else return 0
+        return if(number%n==0 && number%m==0) 1 else 0
     }
 }
