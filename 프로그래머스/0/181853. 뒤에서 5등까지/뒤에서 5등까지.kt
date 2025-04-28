82c37a81e5c67b5b3492b5d19e5b05fd7727cb46
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-04-28 17:03:24 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 31.35 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.kt
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(num_list: IntArray): IntArray{
-        num_list.sort()
-        return num_list.sliceArray(0..4)
+        // num_list.sort()
+        // return num_list.sliceArray(0..4)
+        return num_list.sortedArray().take(5).toIntArray()
     }
 }
