42f4eb4546a9ba8906912b1e09d5ec8d7b207066
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-05-02 23:54:08 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -1,10 +1,15 @@
 class Solution {
     fun solution(n: Int): Int {
+        // var answer = 0
+        // for(x in n.toString()){
+        //     answer += x.digitToInt()
+        // }
+        var input = n
         var answer = 0
-        for(x in n.toString()){
-            answer += x.digitToInt()
+        while(input != 0){
+            answer += input % 10
+            input /= 10
         }
-
         return answer
     }
 }
