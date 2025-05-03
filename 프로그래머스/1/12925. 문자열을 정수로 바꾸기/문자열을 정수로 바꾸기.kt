0261ab82e5415be525f7eaea61d2a92fae9c49de
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-05-03 22:53:02 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.05 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.kt
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.kt
@@ -1,3 +1,5 @@
 class Solution {
-    fun solution(s: String) = if(s[0].isDigit()) s.toInt() else if(s[0] == '+') s.slice(1..s.length-1).toInt() else 0 - s.slice(1..s.length-1).toInt()
+    fun solution(s: String) = 
+    s.toInt()
+    // if(s[0].isDigit()) s.toInt() else if(s[0] == '+') s.slice(1..s.length-1).toInt() else 0 - s.slice(1..s.length-1).toInt()
 }
