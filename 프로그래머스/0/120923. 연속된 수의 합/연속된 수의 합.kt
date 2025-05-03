7425eb8cdbb3bedfbd132fb3c262286572feda09
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-05-03 09:48:32 +0900

    [level 0] Title: 연속된 수의 합, Time: 6.53 ms, Memory: 63.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(num: Int, total: Int): IntArray {
+        var answer = mutableListOf<Int>()
+        if(num % 2 == 0){
+            for(i in 0 until num){
+                answer.add(total/num - num/2 + 1 + i)
+            }
+        }else {
+            for(i in 0 until num){
+                answer.add(total/num - num/2 + i)
+            }
+        }
+        return answer.toIntArray()
+    }
+}
