f24f4de01aca121d6112bf9b9b029acc6ef5a7ce
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-05-05 00:24:49 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 28.14 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.kt
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.kt
@@ -1,3 +1,5 @@
 class Solution {
-    fun solution(numbers: IntArray) = numbers.sorted().let{it[it.lastIndex] * it[it.lastIndex-1]}
+    fun solution(numbers: IntArray) 
+    // = numbers.sorted().let{it[it.lastIndex] * it[it.lastIndex-1]}
+    = numbers.sorted().takeLast(2).fold(1){ acc, i -> acc * i}
 }
