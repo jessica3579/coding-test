a9eefbcf547bba3640204388251fd4f2c5a208f5
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-05-05 16:54:41 +0900

    [level 0] Title: 배열 자르기, Time: 20.39 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.kt
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.kt
@@ -1,3 +1,5 @@
 class Solution {
-    fun solution(numbers: IntArray, num1: Int, num2: Int) = numbers.filterIndexed{ idx, _ -> idx >= num1 && idx <= num2 }
+    fun solution(numbers: IntArray, num1: Int, num2: Int) 
+    // = numbers.filterIndexed{ idx, _ -> idx >= num1 && idx <= num2 }
+    = numbers.sliceArray(num1..num2)
 }
