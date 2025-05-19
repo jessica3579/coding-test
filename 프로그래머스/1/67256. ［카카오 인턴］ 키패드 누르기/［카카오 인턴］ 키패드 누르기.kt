3eb78ffc5c934735436533afaa9d6d749673deb3
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-05-19 23:31:21 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 6.53 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.kt
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.kt
@@ -6,16 +6,22 @@ class Solution {
         var lh = 10
         var rh = 12
         for(x in arr){
-            if(x == 1 || x== 4 || x == 7){
+            if(x % 3 == 1){
                 lh = x
                 answer += "L"
-            }else if(x == 3 || x == 6 || x == 9){
+            }else if(x % 3 == 0){
                 rh = x
                 answer += "R"
             }else{
-                val r = abs(rh-x)/3 + abs(rh-x)%3
-                val l = abs(lh-x)/3 + abs(lh-x)%3
-                if(r == l){
+                val r = distance(rh, x)
+                val l = distance(lh, x)
+                if(r > l){
+                    answer += "L"
+                    lh = x
+                }else if(r < l){
+                    answer += "R"
+                    rh = x
+                }else {
                     if(hand=="right"){
                         answer += "R"
                         rh = x
@@ -23,16 +29,13 @@ class Solution {
                         answer += "L"
                         lh = x
                     }
-                }else if(r > l){
-                    answer += "L"
-                    lh = x
-                }else {
-                    answer += "R"
-                    rh = x
                 }
-                
             }
         }
         return answer
+    }
+    private fun distance(from: Int, to: Int): Int{
+        val diff = abs(from-to)
+        return diff % 3 + diff / 3
     }
 }
