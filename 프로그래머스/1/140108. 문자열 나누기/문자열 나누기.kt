aa1caf6442ebae2a1b61014024f95db29dc09512
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-05-20 17:42:17 +0900

    [level 1] Title: 문자열 나누기, Time: 18.69 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.kt
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.kt
@@ -1,31 +1,55 @@
+// class Solution {
+//     fun solution(s: String): Int {
+//         var answer: Int = 0
+//         var x = s[0]
+//         var xnum = 1
+//         var ynum = 0
+//         var idx = 1
+//         if(s.length == 1) return 1
+//         while(idx < s.length){
+//             if(x == s[idx]) xnum++
+//             else ynum++
+            
+//             if(xnum == ynum && idx == s.length-2){
+//                 answer += 2
+//                 idx++
+//             }else if(xnum == ynum && idx != s.length-1){
+//                 println("if문 안 $idx, $xnum, $ynum")
+//                 answer++
+//                 x = s[idx+1]
+//                 xnum = 1
+//                 ynum = 0
+//                 idx++
+//             }else if(idx == s.length-1) {
+//                 println("else문 안 $idx")
+//                 answer++
+//             }
+//             idx++
+//         }
+//         return answer
+//     }
+// }
+
 class Solution {
     fun solution(s: String): Int {
         var answer: Int = 0
         var x = s[0]
-        var xnum = 1
-        var ynum = 0
-        var idx = 1
+        var xCnt = 0
+        var yCnt = 0
+        var idx = 0
         if(s.length == 1) return 1
-        while(idx < s.length){
-            if(x == s[idx]) xnum++
-            else ynum++
-            
-            if(xnum == ynum && idx == s.length-2){
-                answer += 2
-                idx++
-            }else if(xnum == ynum && idx != s.length-1){
-                println("if문 안 $idx, $xnum, $ynum")
+        for(i in s.indices){
+            if(x == s[i]) xCnt++ else yCnt++
+        
+            if(xCnt == yCnt){
                 answer++
-                x = s[idx+1]
-                xnum = 1
-                ynum = 0
-                idx++
-            }else if(idx == s.length-1) {
-                println("else문 안 $idx")
-                answer++
+                xCnt=0
+                yCnt=0
+                if(i != s.lastIndex) x = s[i+1]
             }
-            idx++
+            
         }
+        if(xCnt != 0 || xCnt != 0) answer++
         return answer
     }
 }
