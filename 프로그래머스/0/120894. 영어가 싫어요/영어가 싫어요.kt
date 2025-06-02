74621e40e7e3751f0452499f0190ca2bfc4702b7
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-02 16:19:03 +0900

    [level 0] Title: 영어가 싫어요, Time: 15.27 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.kt
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.kt
@@ -1,27 +1,51 @@
+// class Solution {
+//     fun solution(numbers: String): Long {
+//         var answer = ""
+//         val list = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+//         var tmp = ""
+//         for(i in 0 .. numbers.length){
+//             if(tmp != "" && list.contains(tmp)){
+//                 when(tmp){
+//                     "zero" -> answer += "0"
+//                     "one" -> answer += "1"
+//                     "two" -> answer += "2"
+//                     "three" -> answer += "3"
+//                     "four" -> answer += "4"
+//                     "five" -> answer += "5"
+//                     "six" -> answer += "6"
+//                     "seven" -> answer += "7"
+//                     "eight" -> answer += "8"
+//                     else -> answer += "9"
+//                 }
+//                 if(i != numbers.length) tmp = "${numbers[i]}"
+//             }else {
+//                 if(i != numbers.length) tmp += numbers[i]
+//             }
+//         }
+//         return answer.toLong()
+//     }
+// }
+
 class Solution {
     fun solution(numbers: String): Long {
-        var answer = ""
-        val list = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-        var tmp = ""
-        for(i in 0 .. numbers.length){
-            if(tmp != "" && list.contains(tmp)){
-                when(tmp){
-                    "zero" -> answer += "0"
-                    "one" -> answer += "1"
-                    "two" -> answer += "2"
-                    "three" -> answer += "3"
-                    "four" -> answer += "4"
-                    "five" -> answer += "5"
-                    "six" -> answer += "6"
-                    "seven" -> answer += "7"
-                    "eight" -> answer += "8"
-                    else -> answer += "9"
-                }
-                if(i != numbers.length) tmp = "${numbers[i]}"
-            }else {
-                if(i != numbers.length) tmp += numbers[i]
-            }
+        var answer = numbers
+        val sample = mapOf(
+            "0" to "zero",
+            "1" to "one",
+            "2" to "two",
+            "3" to "three",
+            "4" to "four", 
+            "5" to "five",
+            "6" to "six",
+            "7" to "seven",
+            "8" to "eight",
+            "9" to "nine"
+        )
+        
+        sample.forEach{
+            answer = answer.replace(it.value, it.key)
         }
+        
         return answer.toLong()
     }
 }
