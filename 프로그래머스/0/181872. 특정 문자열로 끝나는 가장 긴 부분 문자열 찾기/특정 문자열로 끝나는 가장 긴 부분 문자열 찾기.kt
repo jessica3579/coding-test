e975ae0bb2e31760bd3d6fd2cac8cb443d956e9f
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-03 17:01:55 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 10.17 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.kt
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.kt
@@ -1,9 +1,15 @@
+// class Solution {
+//     fun solution(myString: String, pat: String): String {
+//         var lastIdx = 0
+//         val mlen = myString.length
+//         val plen = pat.length
+//         for(i in 0..mlen - plen){
+//             if(myString.substring(i, i + plen) == pat) lastIdx = i
+//         }
+//         return myString.substring(0, lastIdx + plen)
+//     }
+// }
+
 class Solution {
-    fun solution(myString: String, pat: String): String {
-        var lastIdx = 0
-        for(i in 0..myString.length - pat.length){
-            if(myString.substring(i, i + pat.length) == pat) lastIdx = i
-        }
-        return myString.substring(0, lastIdx + pat.length)
-    }
+    fun solution(myString: String, pat: String) = myString.substring(0, myString.lastIndexOf(pat) + pat.length)
 }
