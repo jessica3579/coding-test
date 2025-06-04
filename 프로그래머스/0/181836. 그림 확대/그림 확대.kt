7ff38b93241100a9ea8349eaea834bc9e19a0e6b
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-04 19:22:07 +0900

    [level 0] Title: 그림 확대, Time: 17.57 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(picture: Array<String>, k: Int): Array<String> {
+        var answer = mutableListOf<String>()
+        for(x in picture){
+            var tmp = ""
+            for(y in x){
+                repeat(k){ tmp += y }
+            }
+            repeat(k){ answer.add(tmp) }
+            tmp = ""
+        }
+        return answer.toTypedArray<String>()
+    }
+}
