ef11bda0a150b3a8e52d144c8b208685b23f3fe6
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-05 10:25:31 +0900

    [level 0] Title: 문자열 묶기, Time: 33.59 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.kt
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.kt
@@ -1,10 +1,13 @@
+// class Solution {
+//     fun solution(strArr: Array<String>): Int {
+//         val arr = strArr.map { it.length }
+//         val list = Array(arr.maxOrNull()!! + 1) { 0 }
+//         for(x in arr){
+//             list[x]++
+//         }
+//         return list.maxOrNull()!!
+//     }
+// }
 class Solution {
-    fun solution(strArr: Array<String>): Int {
-        val arr = strArr.map { it.length }
-        val list = Array(arr.maxOrNull()!! + 1) { 0 }
-        for(x in arr){
-            list[x]++
-        }
-        return list.maxOrNull()!!
-    }
+    fun solution(strArr: Array<String>) = strArr.groupBy(String::length).values.maxOf(List<String>::size)
 }
