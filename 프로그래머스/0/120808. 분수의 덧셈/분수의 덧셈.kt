0cb8f004945972e78260c8de28a3c61f36010d40
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-09 17:14:15 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
@@ -1,16 +1,46 @@
+// 방법 1
+// class Solution {
+//     fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
+//         var numer = numer1 * denom2 + numer2 * denom1
+//         var denom = denom1 * denom2
+        
+//         for(x in 2..denom/2){
+//             if(numer % x == 0 && denom % x == 0){
+//                 while(numer % x == 0 && denom % x == 0){
+//                     numer /= x
+//                     denom /= x
+//                 }
+//             }
+//         }
+//         return intArrayOf(numer, denom)
+//     }
+// }
+
+// 방법 2
+// class Solution {
+//     fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
+//         var numer = numer1 * denom2 + numer2 * denom1
+//         var denom = denom1 * denom2
+        
+//         for(x in 2..denom/2){
+//             while(numer % x == 0 && denom % x == 0){
+//                 numer /= x
+//                 denom /= x
+//             }
+//         }
+//         return intArrayOf(numer, denom)
+//     }
+// }
+
+// 방법 3
 class Solution {
+    fun gcd(a: Int, b: Int): Int = if(b == 0) a else gcd(b, a % b)
     fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
         var numer = numer1 * denom2 + numer2 * denom1
-        var denom = denom1 * denom2 
-        // 분모의 인자로 분수를 나눌 수 있으면 나누기
-        for(x in 2..denom/2){
-            if(numer % x == 0 && denom % x == 0){
-                while(numer % x == 0 && denom % x == 0){
-                    numer /= x
-                    denom /= x
-                }
-            }
-        }
-        return intArrayOf(numer, denom)
+        var denom = denom1 * denom2
+        
+        val gcdValue = gcd(numer, denom)
+        
+        return intArrayOf(numer / gcdValue, denom / gcdValue)
     }
 }
