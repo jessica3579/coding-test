2415ac2c18138b120a5bb78ef115e8c6503f4704
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-11 17:35:17 +0900

    [level 1] Title: 실패율, Time: 93.21 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.kt
+++ 프로그래머스/1/42889. 실패율/실패율.kt
@@ -1,3 +1,25 @@
+// class Solution {
+//     fun solution(N: Int, stages: IntArray): IntArray {
+//         var sNum: MutableMap<Int, Int> = mutableMapOf()
+//         var fPct: MutableMap<Int, Double> = mutableMapOf()
+
+//         for(x in stages){
+//             sNum.put(x, sNum.getOrDefault(x, 0) + 1)
+//         }
+//         var players = stages.size
+//         for(i in 1..N){
+//             val notClearP = sNum.getOrDefault(i, 0)
+//             if(players != 0){
+//                 fPct.put(i, notClearP / players.toDouble())
+//             }else {
+//                 fPct.put(i, 0.0)
+//             }
+//             players -= notClearP
+//         }
+//         // return fPct.entries.sortedByDescending{ it.value }.map{ it.key }.toIntArray()
+//         return fPct.entries.sortedWith( compareByDescending<Map.Entry<Int, Double>> { it.value }.thenBy{ it.key } ).map{ it.key }.toIntArray()
+//     }
+// }
 class Solution {
     fun solution(N: Int, stages: IntArray): IntArray {
         var sNum: MutableMap<Int, Int> = mutableMapOf()
@@ -6,14 +28,12 @@ class Solution {
         for(x in stages){
             sNum.put(x, sNum.getOrDefault(x, 0) + 1)
         }
+        
         var players = stages.size
         for(i in 1..N){
             val notClearP = sNum.getOrDefault(i, 0)
-            if(players != 0){
-                fPct.put(i, notClearP / players.toDouble())
-            }else {
-                fPct.put(i, 0.0)
-            }
+            var fail = if(players == 0) 0.0 else notClearP / players.toDouble()
+            fPct.put(i, fail)
             players -= notClearP
         }
         // return fPct.entries.sortedByDescending{ it.value }.map{ it.key }.toIntArray()
