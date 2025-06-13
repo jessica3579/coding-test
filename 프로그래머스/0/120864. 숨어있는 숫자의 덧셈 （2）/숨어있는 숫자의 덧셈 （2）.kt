bfe14cb5fb17dc88e2f8f76d6b25bb3fc86d133f
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-13 17:25:14 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 16.74 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.kt
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.kt
@@ -1,3 +1,10 @@
+// class Solution {
+//     fun solution(my_string: String) = my_string.map { if(it.isDigit()) it else ' ' }.joinToString("").split(' ').filter{ it.isNotEmpty() }.map{ it.toInt() }.sum()
+// }
+
 class Solution {
-    fun solution(my_string: String) = my_string.map { if(it.isDigit()) it else ' ' }.joinToString("").split(' ').filter{ it.isNotEmpty() }.map{ it.toInt() }.sum()
+    fun solution(my_string: String): Int  = 
+    my_string.split("[A-z]+".toRegex())
+        .filter(String::isNotEmpty)
+        .sumOf(String::toInt)
 }
