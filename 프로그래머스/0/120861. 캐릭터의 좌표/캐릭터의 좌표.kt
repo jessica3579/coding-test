7a736b4df31e72556a08517a17d721de2ca411f1
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-17 23:48:17 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun canMove(key: String, board: IntArray, location: IntArray): Boolean{
+        val horizon = board[0] / 2 
+        val verti = board[1] / 2 
+        var value = false
+        when(key){
+            "left" -> if( location[0] > 0 - horizon ) value = true
+            "right" -> if( location[0] < horizon ) value = true
+            "up" -> if( location[1] < verti ) value = true
+            "down" -> if( location[1] > 0 - verti ) value = true
+        }
+        return value
+    }
+    fun solution(keyinput: Array<String>, board: IntArray): IntArray {
+        var location = intArrayOf(0, 0)
+        keyinput.forEach{ key ->
+            val cM = canMove(key, board, location)
+            when(key){
+                "left" -> if(cM) location[0] -= 1
+                "right" -> if(cM) location[0] += 1
+                "up" -> if(cM) location[1] += 1
+                "down" -> if(cM) location[1] -= 1
+            }
+        }
+        return location
+    }
+}
