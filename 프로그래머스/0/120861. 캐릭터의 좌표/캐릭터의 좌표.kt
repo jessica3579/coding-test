df94a3167c8fbdf9787c52d8ea2db20750078999
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-17 23:59:28 +0900

    Update 캐릭터의 좌표.kt

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.kt
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.kt
@@ -1,26 +1,48 @@
+// class Solution {
+//     fun canMove(key: String, board: IntArray, location: IntArray): Boolean{
+//         val horizon = board[0] / 2 
+//         val verti = board[1] / 2 
+//         var value = false
+//         when(key){
+//             "left" -> if( location[0] > 0 - horizon ) value = true
+//             "right" -> if( location[0] < horizon ) value = true
+//             "up" -> if( location[1] < verti ) value = true
+//             "down" -> if( location[1] > 0 - verti ) value = true
+//         }
+//         return value
+//     }
+//     fun solution(keyinput: Array<String>, board: IntArray): IntArray {
+//         var location = intArrayOf(0, 0)
+//         keyinput.forEach{ key ->
+//             val cM = canMove(key, board, location)
+//             when(key){
+//                 "left" -> if(cM) location[0] -= 1
+//                 "right" -> if(cM) location[0] += 1
+//                 "up" -> if(cM) location[1] += 1
+//                 "down" -> if(cM) location[1] -= 1
+//             }
+//         }
+//         return location
+//     }
+// }
+
 class Solution {
-    fun canMove(key: String, board: IntArray, location: IntArray): Boolean{
+    fun move(key: String, board: IntArray, location: IntArray): IntArray{
         val horizon = board[0] / 2 
         val verti = board[1] / 2 
-        var value = false
+        var location = location
         when(key){
-            "left" -> if( location[0] > 0 - horizon ) value = true
-            "right" -> if( location[0] < horizon ) value = true
-            "up" -> if( location[1] < verti ) value = true
-            "down" -> if( location[1] > 0 - verti ) value = true
+            "left" -> if( location[0] > 0 - horizon ) location[0] -= 1
+            "right" -> if( location[0] < horizon ) location[0] += 1
+            "up" -> if( location[1] < verti ) location[1] += 1
+            "down" -> if( location[1] > 0 - verti ) location[1] -= 1
         }
-        return value
+        return location
     }
     fun solution(keyinput: Array<String>, board: IntArray): IntArray {
         var location = intArrayOf(0, 0)
         keyinput.forEach{ key ->
-            val cM = canMove(key, board, location)
-            when(key){
-                "left" -> if(cM) location[0] -= 1
-                "right" -> if(cM) location[0] += 1
-                "up" -> if(cM) location[1] += 1
-                "down" -> if(cM) location[1] -= 1
-            }
+            val location = move(key, board, location)
         }
         return location
     }
