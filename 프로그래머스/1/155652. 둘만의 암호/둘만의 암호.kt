ba1ad5578eb48138f7c980a50bc2848e8d9a2d18
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-20 00:23:38 +0900

    [level 1] Title: 둘만의 암호, Time: 27.88 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
@@ -21,15 +21,42 @@
 //     }
 // }
 
+// class Solution {
+//     fun solution(s: String, skip: String, index: Int): String {
+//         val alphabet = ('a'..'z').filter { it !in skip }  // skip 제외한 알파벳 리스트
+//         val map = alphabet.withIndex().associate { it.value to it.index }  // 문자 → 인덱스 매핑
+//         val size = alphabet.size
+
+//         return s.map { ch ->
+//             val pos = map[ch]!!  // 현재 문자의 인덱스
+//             alphabet[(pos + index) % size]  // index만큼 이동한 뒤, 순환
+//         }.joinToString("")
+//     }
+// }
+
 class Solution {
     fun solution(s: String, skip: String, index: Int): String {
-        val alphabet = ('a'..'z').filter { it !in skip }  // skip 제외한 알파벳 리스트
-        val map = alphabet.withIndex().associate { it.value to it.index }  // 문자 → 인덱스 매핑
-        val size = alphabet.size
-
-        return s.map { ch ->
-            val pos = map[ch]!!  // 현재 문자의 인덱스
-            alphabet[(pos + index) % size]  // index만큼 이동한 뒤, 순환
+        val alphabet = ('a'..'z').filter{ it !in skip }
+        val map = alphabet.withIndex().associate{ it.value to it.index }
+        
+        return s.map { c ->
+            alphabet[(map[c]!! + index) % alphabet.size]
         }.joinToString("")
     }
 }
+
+
+
+// class Solution {
+//     fun solution(s: String, skip: String, index: Int): String {
+//         var answer: String = ""
+//         val skipped = skip.map { it.toChar() }
+//         val alphabet = ('a'..'z').filter { it !in skipped }
+
+//         s.forEach {
+//             val i = (alphabet.indexOf(it) + index) % alphabet.size
+//             answer += alphabet[i]
+//         }
+//         return answer
+//     }
+// }
