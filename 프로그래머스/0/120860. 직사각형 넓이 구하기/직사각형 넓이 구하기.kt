81ceb8bc73ebf1ac4b5b07f60b74c8cc4d0d371d
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-06-20 17:26:45 +0900

    Update 직사각형 넓이 구하기.kt

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
@@ -1,14 +1,27 @@
+// class Solution {
+//     fun solution(dots: Array<IntArray>): Int {
+//         val x1 = dots[0][0]
+//         val y1 = dots[0][1]
+        
+//         val x2 = if(dots[1][1] == y1) dots[1][0] else if(dots[2][1] == y1) dots[2][0] else dots[3][0]
+//         val y2 = if(dots[1][0] == x1) dots[1][1] else if(dots[2][0] == x1) dots[2][1] else dots[3][1]
+        
+//         val width = maxOf(x1, x2) - minOf(x1, x2) 
+//         val height = maxOf(y1, y2) - minOf(y1, y2) 
+    
+//         return width * height
+//     }
+// }
+
 class Solution {
     fun solution(dots: Array<IntArray>): Int {
-        var x1 = dots[0][0]
-        var y1 = dots[0][1]
+        val xValues = dots.map{ it[0] }
+        val yValues = dots.map{ it[1] }
         
-        var x2 = if(dots[1][1] == y1) dots[1][0] else if(dots[2][1] == x1) dots[2][0] else dots[3][0]
-        var y2 = if(dots[1][0] == x1) dots[1][1] else if(dots[2][0] == x1) dots[2][1] else dots[3][1]
-        
-        val width = maxOf(x1, x2) - minOf(x1, x2) 
-        val height = maxOf(y1, y2) - minOf(y1, y2) 
+        val width = xValues.maxOrNull()!! - xValues.minOrNull()!!
+        val height = yValues.maxOrNull()!! - yValues.minOrNull()!!
     
         return width * height
     }
 }
+
