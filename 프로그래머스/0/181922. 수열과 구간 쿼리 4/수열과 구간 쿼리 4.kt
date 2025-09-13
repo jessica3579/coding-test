d64f6a61645e4167ffd6dd1ae728d51527294463
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-09-13 23:28:48 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.32 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
+        for((s, e, k) in queries){
+            for(i in s..e){
+                if(i % k == 0){
+                    arr[i]++
+                }
+            }
+        }
+        return arr
+    }
+}
