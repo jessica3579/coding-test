a1a5b724b9e688b779d3085776b3aed11df14571
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2025-10-15 23:49:56 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
+        var idx1 = 0 
+        var idx2 = 0 
+        goal.forEach {
+            if (idx1 < cards1.size && it == cards1[idx1]) idx1++ 
+            else if (idx2 < cards2.size && it == cards2[idx2]) idx2++ 
+            else return "No" 
+        }
+        return "Yes"
+    }
+}
