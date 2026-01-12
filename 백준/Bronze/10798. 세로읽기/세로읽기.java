af6050ef8bcab84b0298facfad9ceec21fe4b9bd
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2026-01-12 23:14:09 +0900

    [Bronze I] Title: 세로읽기, Time: 164 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        String[] arr = new String[5];
-        
-        for(int i=0; i<5; i++){
-            arr[i] = sc.next();
-        }
-        
-        for(int col=0; col<15; col++){
-            for(int row=0; row<5; row++){
-                if(col < arr[row].length()) System.out.print(arr[row].charAt(col));
-            }
-        }
-        
-    }
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char[][] c_arr = new char[5][15];
+		int i,j;
+		int max = 0;
+		
+		for(i=0;i<5;i++) {
+			String str = sc.next();
+			
+			if(str.length()>max) 
+				max = str.length();
+			
+			for(j=0;j<str.length();j++) {
+				c_arr[i][j] = str.charAt(j);
+			}
+		}
+		
+		
+		for(i=0;i<max;i++) {
+			for(j=0;j<5;j++) {
+				if(c_arr[j][i] == '\0') 
+					continue;
+				System.out.print(c_arr[j][i]);
+			}
+		}
+		sc.close();
+	}
 }
