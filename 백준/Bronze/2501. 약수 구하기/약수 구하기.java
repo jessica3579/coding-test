6dda52f024a7955ee1413b2e3adac61460efe606
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2026-01-14 23:56:18 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,21 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int index = sc.nextInt();
-        int count = 0;
-        
-        for(int i=1; i<=n; i++){
-            if(n % i == 0) count++;
-            if(count == index){
-                System.out.print(i);
-                return;
-            }
-        }
-        System.out.print(0);
-        
-    }
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int count = 0;
+		int result = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			if(N % i == 0) {
+				count++;
+			}
+			if(count == K) {
+				result = i;
+				break;
+			}
+		}
+		System.out.println(result);
+	}
+
 }
