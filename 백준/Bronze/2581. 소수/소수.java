b8c034df4e207bbd3cbd41807bbe0affedda3631
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2026-01-18 22:46:20 +0900

    [Bronze II] Title: 소수, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,34 +1,54 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-    static boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-
-        int sum = 0;
-        int min = -1;
-
-        for (int i = M; i <= N; i++) {
-            if (isPrime(i)) {
-                sum += i;
-                if (min == -1) min = i;
-            }
-        }
-
-        if (min == -1) {
-            System.out.println(-1);
-        } else {
-            System.out.println(sum);
-            System.out.println(min);
-        }
-    }
+ 
+	public static boolean prime[];
+	
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+		prime = new boolean[N + 1];	
+		get_prime();
+		
+		
+		// 소수 합 및 최솟값 
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for(int i = M; i <= N; i++) {
+			if(prime[i] == false) {	
+				sum += i;
+				if(min == Integer.MAX_VALUE) {	
+					min = i;
+				}
+			}
+		}
+		
+		if(sum == 0) {	
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+		
+	}
+ 
+	public static void get_prime() {
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;	
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+		
+	}
 }
