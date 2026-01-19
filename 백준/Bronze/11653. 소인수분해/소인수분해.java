fc67dcb854c709236687d02efe991cbc75dd2be0
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2026-01-19 23:52:44 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        
-        while(n != 1){
-            for(int i=2; i<=n; i++){
-                if(n % i == 0){
-                    n /= i;
-                    System.out.println(i);
-                    break;
-                }
-            }
-        }
-    }
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+ 
+		for (int i = 2; i <= Math.sqrt(N); i++) {
+			while (N % i == 0) {
+				sb.append(i).append('\n');
+				N /= i;
+			}
+		}
+		if (N != 1) {
+			sb.append(N);
+		}
+		System.out.println(sb);
+	}
 }
