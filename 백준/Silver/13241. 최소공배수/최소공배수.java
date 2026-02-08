e8d9e17fd4ee1dd39a5bb6991a2d8ffe4e91efae
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2026-02-08 23:17:42 +0900

    [Silver V] Title: 최소공배수, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static long gcd(long a, long b)
+    {
+        if(b==0)
+        {
+            return a;
+        }
+        else
+        {
+            return gcd(b,a%b);
+        }
+    }
+    
+    public static long lcm(long a, long b)
+    {
+        long gcd = gcd(a,b);
+        return (a*b)/gcd;
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        
+        System.out.println(lcm(a,b));
+       
+
+    }
+}
